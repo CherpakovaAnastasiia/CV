452bdacc4c838faaf5e0ce459252fbe6da2a3b34
Cherpakova Anastasiia <13022009a@gmail.com>
2023-10-15 11:09:08 +0200

    target-modal

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,34 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+// // Нужно доделать!!!!!
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+// document.addEventListener("DOMContentLoaded", function() {
+//   const contactModal = document.getElementById('contact-modal');
+//   const modalOpenButton = document.querySelector('[data-modal-open]');
+//   const modalCloseButton = document.getElementById('modal-close');
+//   const modalCheckbox = document.getElementById('modal-checkbox');
+
+//   modalOpenButton.addEventListener('click', () => {
+//     contactModal.style.display = 'block';
+//   });
+
+//   modalCloseButton.addEventListener('click', () => {
+//     contactModal.style.display = 'none';
+//   });
+
+//   window.addEventListener('click', (event) => {
+//     if (event.target === contactModal) {
+//       contactModal.style.display = 'none';
+//     }
+//   });
+
+//   modalCheckbox.addEventListener('change', () => {
+//     if (modalCheckbox.checked) {
+//       contactModal.style.display = 'block';
+//     } else {
+//       contactModal.style.display = 'none';
+//     }
+//   });
+// });
+
+
+1. Не отображается закрывающаяся иконка-крестик. 
